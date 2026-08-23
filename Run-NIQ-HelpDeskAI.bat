24f2b3117c2e35280cef8833109133b95ac1fdec
Michael1069 <mikemicky05@gmail.com>
2026-08-23 14:08:48 +0530

    Build standalone Windows executable and setup installer in release folder

--- Run-NIQ-HelpDeskAI.bat
+++ Run-NIQ-HelpDeskAI.bat
@@ -1,5 +1,10 @@
 @echo off
 title NIQ HelpDeskAI - Enterprise IT Support
-echo Starting NIQ HelpDeskAI Desktop Software...
+echo Starting NIQ HelpDeskAI Standalone Desktop Application...
 cd /d "%~dp0"
-npx electron .
+
+if exist "release\win-unpacked\NIQ HelpDeskAI.exe" (
+    start "" "release\win-unpacked\NIQ HelpDeskAI.exe"
+) else (
+    npx electron .
+)
